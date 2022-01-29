1bd50cea76e49b41b132451b23720c4b9d3c8dc8
Bryce Crist <brucecrusty@gmail.com>
2022-01-28 18:11:53 -0700

    Added todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,17 @@
+// TODO: Create a basic game map -- 2D array?
+    // TODO: Game Map -- dynamically created
+// TODO: Create Sprite class
+// TODO: Create entities -- each entity will have a sprite
+// TODO: Create player class
+    // TODO: Create movement in the player class
+    // TODO: Create basic wall collision -- maybe?
+    // TODO: Create entity/sprite collision
+// TODO: Enemy creation
+// TODO: Create user input system
+// TODO: Combat logic
+// TODO: Create items?
+// TODO: Create basic inventory 
+
 public class Main {
 
     public static void main(String[] args) {
